a424a728e862efecbdda3cd483748a5c5bb64975
Thijs de Zoete <thijsdezoete@gmail.com>
2013-08-30 15:55:36 +0200

    Fix mysql implementation and added default config

--- mysql_statsd/mysql_statsd.py
+++ mysql_statsd/mysql_statsd.py
@@ -7,6 +7,7 @@ import signal
 import sys
 import threading
 import time
+from ConfigParser import ConfigParser
 
 from thread_manager import ThreadManager
 from thread_mysql import ThreadMySQL
@@ -24,24 +25,38 @@ class MysqlStatsd():
 
         op.add_argument("-c", "--config", dest="file", default="/etc/mysql-statsd.conf", help="Configuration file")
         op.add_argument("-d", "--debug", dest="debug", help="Debug mode", default=False, action="store_true")
+        op.add_argument("-q", "--query-interval", dest="query_interval", default=1000, help="Interval to poll mysql for new data in miliseconds")
 
         self.opt = op.parse_args()
         opt = self.opt
+
+        self.get_config(opt.file)
 
         # Set up queue
         self.queue = Queue.Queue()
 
         # Spawn MySQL polling thread
-        t1 = ThreadMySQL(config=self.config, queue=self.queue)
+
+        t1 = ThreadMySQL(queue=self.queue, **self.config['mysql'])
+        # t1 = ThreadMySQL(config=self.config, queue=self.queue)
 
         # Spawn Statsd flushing thread
-        t2 = ThreadStatsd(config=self.config, queue=self.queue)
+        t2 = ThreadStatsd(queue=self.queue, **self.config['statsd'])
 
         # Get thread manager
         tm = ThreadManager(threads=[t1, t2])
-
         tm.run()
 
+    def get_config(self, config_file):
+        cnf = ConfigParser()
+        cnf.read(config_file)[0]
+        self.config = {}
+        for section in cnf.sections():
+            self.config[section] = {}
+            for key, value in cnf.items(section):
+                self.config[section][key] = value
+
+        return self.config
 
 if __name__ == '__main__':
     program = MysqlStatsd()
