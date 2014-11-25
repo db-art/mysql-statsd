7a8725534d7b83598e43d63978f8fc49a3b95f16
Giuliano <giuliano@108.bz>
2014-11-25 11:13:36 +0000

    Support for SHOW SLAVE STATUS via a ColumnsPreprocessor

--- mysql_statsd/preprocessors/mysql_preprocessor.py
+++ mysql_statsd/preprocessors/mysql_preprocessor.py
@@ -5,5 +5,5 @@ class MysqlPreprocessor(Preprocessor):
     def __init__(self, *args, **kwargs):
         super(MysqlPreprocessor, self).__init__(*args, **kwargs)
 
-    def process(self, rows):
+    def process(self, column_names, rows):
         return list(rows)
