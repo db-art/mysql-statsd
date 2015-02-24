8397f9c774c3ae3be1c042eea2b38083c3a0afb7
Thijs de Zoete <thijsdezoete@gmail.com>
2015-02-24 15:56:34 +0100

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='mysql-statsd',
-    version='0.1.2',
+    version='0.1.4',
     description='Daemon that gathers statistics from MySQL and sends them to statsd.',
     long_description=readme + '\n\n' + history,
     author='Jasper Capel, Thijs de Zoete',
